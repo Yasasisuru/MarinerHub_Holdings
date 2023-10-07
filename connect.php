4748624649612198523f76a733e9aa084201066c
Yasas_Isuru <yasasisuru2@gmail.com>
2023-10-07 22:09:21 +0530

    php file design

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php  
+
+
+$Company = $_POST['Company'];
+$imo = $_POST['imo'];
+$Person = $_POST['Person'];
+$Email = $_POST['Email'];
+$Phone = $_POST['Phone'];
+
+$Vessel = $_POST['Vessel'];
+$vessel_type = $_POST['vessel_type'];
+$location = $_POST['location'];
+$captain = $_POST['captain'];
+
+$length = $_POST['length'];
+$width = $_POST['width'];
+$Draft = $_POST['Draft'];
+
+$repair = $_POST['repair'];
+$Specific_Areas = $_POST['Specific_Areas'];
+$Draft = $_POST['Draft'];
+$optradio = $_POST['optradio'];
+
+?>
